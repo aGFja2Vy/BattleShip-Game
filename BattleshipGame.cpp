125a1d72887e04b43296e3e12f6c2def447365d3
RaveMaster073 <47868518+RaveMaster073@users.noreply.github.com>
2019-02-21 12:40:08 -0800

    +changes to shoot()

--- BattleshipGame.cpp
+++ BattleshipGame.cpp
@@ -3,18 +3,21 @@
 #include <ctime>
 void shoot();
 bool check_hit(int x, int y);
+void board_change(int x, int y);
 int main()
 {
 	return 0;
 }
-void shoot()
+void shoot(char coordinatex, char coordinatey)
 {
-	char coordinatex;
-	char coordinatey;
-	std::cout << "Where would you like to aim?\n"
-
+	std::cout << "Where would you like to aim?\n";
 	std::cin >> coordinatex;
 	std::cin >> coordinatey;
+	bool check_hit = check_hit(coordinatex, coordinatey);
+	if (check_hit)
+		array[coordinatex][coordinatey] = 'H';
+	else
+		array[coordinatex][coordinatey] = 'X';
 }
 bool check_hit(int x, int y)
 {
