369966ee8f0a32002d09ef1155d266778f645763
RaveMaster073 <47868518+RaveMaster073@users.noreply.github.com>
2019-02-23 22:02:12 -0800

    +hspace_test and vspace_test
    alright, i figured it out. it looks good too, might have to talk about possible code shortenings.

--- BattleShipGame/BattleShipGame.cpp
+++ BattleShipGame/BattleShipGame.cpp
@@ -1,10 +1,14 @@
-// BattleShipGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// BattleShipGame.cpp : This file contains the 'main' function. Program e// BattleShipGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //#include <cstdlib> to create random inputs by the "ai".
 //#include <ctime> to create truly random numbers with the <cstdlib> random functions.
-//shoot() function recieves input from the player and inputs into the check_hit() function, and outputs whether or not the shot hit or missed.
-//check_hit() function checks the board for whether or not the input was successful or not, and outputs the answer to the shoot() function.
+//DisplayBoard() to show all of the player's board values to them.
 //set_board() to initially set all values of the board to O.
-//Displayboard() to show all of the player's board values to them.
+//set_ship() to set up the player's ships with their inputs. Also resets int values so it may be used again.
+//shoot() to recieve input from the player and input into the check_hit() function, and outputs whether or not the shot hit or missed.
+//check_hit() to check the board for whether or not the input was successful or not, and outputs the answer to the shoot() function.
+//life_test() to check if any of a player's ships are alive. Will end the game if no ship spaces are found on either board.
+//hspace_test() to check if places chosen by the player for their ships will overlap other ships and/or clip out of the board for horizontal orientation.
+//vspace_test() to check if places chosen by the player for their ships will overlap other ships and/or clip out of the board for vertical orientation.
 //main() to start the thing.
 
 //#include <cstdlib> to create random inputs by the "ai".
@@ -20,14 +24,23 @@ void DisplayBoard(char board[15][15]);
 //set_board() to initially set all values of the board to O.
 void set_board(char board[15][15]);
 
+//set_ship() to set up the player's ships with their inputs. Also resets int values so it may be used again.
+void set_ship(char board[15][15], int x2 = 0, int y2 = 0);
+
 //shoot() to recieve input from the player and input into the check_hit() function, and outputs whether or not the shot hit or missed.
 void shoot(char coordinatex, char coordinatey, char board[15][15]);
 
 //check_hit() to check the board for whether or not the input was successful or not, and outputs the answer to the shoot() function.
 bool check_hit(int x, int y, char board[15][15]);
 
-//life_test to check if any of a player's ships are alive. Usage will be in a while loop that will finish the game if all of one's ships have died. Undecided function name.
+//life_test() to check if any of a player's ships are alive. Will end the game if no ship spaces are found on either board.
 bool life_test(char board[15][15]);
+
+//hspace_test() to check if places chosen by the player for their ships will overlap other ships and/or clip out of the board for horizontal orientation.
+bool hspace_test(char board[15][15], int x2, int y2);
+
+//vspace_test() to check if places chosen by the player for their ships will overlap other ships and/or clip out of the board for vertical orientation.
+bool vspace_test(char board[15][15], int x2, int y2);
 
 //main() to start the thing.
 int main()
@@ -55,7 +68,6 @@ void DisplayBoard(char board[15][15])
 		}
 		std::cout << std::endl;
 	}
-
 }
 
 void set_board(char board[15][15])
@@ -68,37 +80,101 @@ void set_board(char board[15][15])
 	}
 }
 
+//Unfortunately, string variables don't seem to work with std::cin. For now, setting a number for horizontal or vertical position.
+void set_ship(char board[15][15], int x2, int y2)
+{
+	bool ship_settle = false;
+	do {
+		bool hspacetest;
+		bool vspacetest;
+		x2 = 0;
+		y2 = 0;
+		int position = 0;
+		std::cout << "Where would you like to place your ship?" << std::endl;
+		std::cin >> x2 >> y2;
+		std::cout << "1: Horizontal \n2: Vertical" << std::endl;
+		std::cin >> position;
+		if (position == 1) {
+			hspacetest = hspace_test(board, x2, y2);
+			if (hspacetest) {
+				for (int y = 0; y < 5; y++) {
+					board[x2][y2 - 2 + y] = '#';
+				}
+				ship_settle = false;
+			}
+		}
+		else {
+			vspacetest = vspace_test(board, x2, y2);
+			if (vspacetest) {
+				for (int x = 0; x < 5; x++) {
+					board[x2 - 2 + x][y2] = '#';
+				}
+				ship_settle = false;
+			}
+		}
+		if (!(hspacetest) || !(vspacetest)) {
+			std::cout << "Invalid position.";
+			ship_settle = true;
+		}
+	} while (ship_settle);
+}
+
 //For now, undecided variables for shoot() function. Suggesting variable usage of x1 and y1.
-void shoot(char coordinatex, char coordinatey, char board[15][15])
+void shoot(char x1, char y1, char board[15][15])
 {
 	std::cout << "Where would you like to aim?\n";
-	std::cin >> coordinatex;
-	std::cin >> coordinatey;
-	bool hit = check_hit(coordinatex, coordinatey, board);
+	std::cin >> x1 >> y1;
+	bool hit = check_hit(x1, y1, board);
 	if (hit)
-		board[coordinatex][coordinatey] = 'H';
+		board[x1][y1] = 'H';
 	else
-		board[coordinatex][coordinatey] = 'X';
+		board[x1][y1] = 'X';
 }
 
 bool check_hit(int x, int y, char board[15][15])
 {
-	if (board[x][y] = '#')
+	if (board[x][y] == '#')
 		return true;
 	return false;
 }
 
-//Reiteration: function name "life_test" undecided. Also requesting possible shortening of code.
+//function name "life_test" undecided. Also requesting possible shortening of code.
 bool life_test(char board[15][15])
 {
 	int lifetest = 0;
 	for (int x = 0; x < 15; x++) {
 		for (int y = 0; y < 15; y++) {
-			if(board[x][y] = '#')
-			lifetest++;
+			if (board[x][y] == '#')
+				lifetest++;
 		}
 	}
 	if (lifetest >= 1)
+		return true;
+	return false;
+}
+
+//So far, the space tests will only be able to detect space for a 5-space ship. We will need to change this in the future to work with smaller ships.
+bool hspace_test(char board[15][15], int x2, int y2)
+{
+	int hspacetest = 0;
+	for (int y = 0; y < 5; y++) {
+		if (board[x2][y2 - 2 + y] == 'O') {
+			hspacetest++;
+		}
+	}
+	if (hspacetest == 5)
+		return true;
+	return false;
+}
+bool vspace_test(char board[15][15], int x2, int y2)
+{
+	int vspacetest = 0;
+	for (int x = 0; x < 5; x++) {
+		if (board[x2 - 2 + x][y2] == 'O') {
+			vspacetest++;
+		}
+	}
+	if (vspacetest == 5)
 		return true;
 	return false;
 }
@@ -113,3 +189,4 @@ bool life_test(char board[15][15])
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
